2896a340cf00da30e86de0d0dc192b54366e1e9a
Estiven Mejia <mejiaguetoestiven@gmail.com>
2025-06-15 15:31:36 -0500

    Full Stack Open Part 4

--- bloglist/app.js
+++ bloglist/app.js
@@ -13,6 +13,11 @@ const {
   tokenExtractor,
   userExtractor,
 } = require("./utils/middleware");
+const User = require('./models/user')
+const Blog = require('./models/blog')
+const bcrypt = require("bcryptjs");
+const initialBlogs = require('./utils/initial').initialBlogs
+const initialUsers = require('./utils/initial').initialUsers
 
 const app = express();
 mongoose.set("strictQuery", false);
@@ -26,7 +31,41 @@ app.use(requestLogger);
 app.use("/api/blogs", userExtractor, blogRouter);
 app.use("/api/users", userRouter);
 app.use("/api/login", loginRouter);
+app.use("/api/testing/reset", async ( request, response ) => {
+  // This endpoint is for testing purposes only
+  // It will delete all blogs and users from the database
+  await User.deleteMany({});
+  await Blog.deleteMany({});
+  response.status(204).end();
+})
 app.use(unknownEndpoint);
 app.use(errorHandler);
+
+(async () => {
+  try{
+    await Blog.deleteMany({});
+    await User.deleteMany({});
+  
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(initialUsers.password, saltRounds);
+    const user = new User({
+      username: initialUsers.username,
+      name: initialUsers.name,
+      passwordHash,
+    });
+  
+    const savedUser = await user.save();
+    const blogsObjects = initialBlogs.map(
+      (blog) => new Blog({ ...blog, user: savedUser.id })
+    );
+    const blogsSaved = blogsObjects.map((e) => e.save());
+    await Promise.all(blogsSaved);
+    console.log("Connected to MongoDB and initial data loaded");
+  }catch(error){
+    console.log("Error connecting to MongoDB:", error.message);
+    
+  }
+  
+})();
 
 module.exports = app;
--- bloglist/index.js
+++ bloglist/index.js
@@ -4,6 +4,7 @@ const {PORT} = require("./utils/config")
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`API running on http://localhost:${PORT}/api`);
 })
 
 module.exports = app
